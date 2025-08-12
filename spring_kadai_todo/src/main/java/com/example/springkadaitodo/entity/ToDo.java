f0ddbe62b495660cc7a899e34099c47b4d246ad7
babios08 <0tv37027729305v@ezweb.ne.jp>
2025-08-12 18:38:04 +0900

    Add files via upload

---
+++ spring_kadai_todo/src/main/java/com/example/springkadaitodo/entity/ToDo.java
@@ -0,0 +1,34 @@
+package com.example.springkadaitodo.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // このクラスがDBのテーブルと対応するよという印
+@Table(name = "todos")
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String priority; // "高" "中" "低"
+    private String status;   // "未着手" "着手中" "完了"
+
+    // getter & setter
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getPriority() { return priority; }
+    public void setPriority(String priority) { this.priority = priority; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
